e87f0624b589e24804de42e883d0baf64bd27abf
Borja <jesdoborja@gmail.com>
2024-01-24 08:57:12 +0100

    Arreglado el requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:borja@localhost/flaskmysql'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.app_context().push()
